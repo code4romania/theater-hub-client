38b75a3ce86390a1c39b0b4b7e9e94a59bb8c49a
AndreiMitrea <andrei.mitrea@imbuelabs.com>
2019-08-29 10:36:05 +0300

    Uncommented get locales middleware

--- middleware/get-locales.js
+++ middleware/get-locales.js
@@ -1,8 +1,8 @@
 
 export default ({ store, redirect }) => {
-    // if (store.getters['applicationData/locales'].length === 0) {
-    //     return store.dispatch('applicationData/getLocales').then(() => {
-    //         store.dispatch('setLocale', store.getters['applicationData/locales'][0].ID);
-    //     });
-    // }
+    if (store.getters['applicationData/locales'].length === 0) {
+        return store.dispatch('applicationData/getLocales').then(() => {
+            store.dispatch('setLocale', store.getters['applicationData/locales'][0].ID);
+        });
+    }
 }
