629bfb0ead634b95ff348ef99833f26c66909bb0
AndreiMitrea <andrei.mitrea@imbuelabs.com>
2019-08-30 16:43:40 +0300

    Updated logged user layout

--- config/env/common.js
+++ config/env/common.js
@@ -1,13 +1,16 @@
 export const commonConfig = {
     application: {
-        baseURL: 'http://localhost:3000',
-        homeLink: 'https://code4.ro/{0}/',
+        baseURL: 'http://localhost:3000'
+    },
+    API: {
+        baseURL: 'http://127.0.0.1:8081/api'
+    },
+    CodeForRomania: {
+        homeLinkRO: 'https://code4.ro/ro/',
+        homeLinkEN: 'https://code4.ro/en/',
         donationLinkRO: 'https://code4.ro/ro/doneaza/',
         donationLinkEN: 'https://code4.ro/en/donate/',
         aboutLinkRO: 'https://code4.ro/ro/cine-suntem/',
         aboutLinkEN: 'https://code4.ro/en/who-we-are/'
-    },
-    API: {
-        baseURL: 'https://localhost:443/api'
     }
  };
